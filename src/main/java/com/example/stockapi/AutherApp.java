8396f03b1d1fbce40dcc2d60d11a764618b7483f
thecoderenroute <thecoderenroute@gmail.com>
2022-04-27 19:01:55 +0530

    Minor change

--- src/main/java/com/example/stockapi/AutherApp.java
+++ src/main/java/com/example/stockapi/AutherApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StockApiApplication {
+public class AutherApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(StockApiApplication.class, args);
+        SpringApplication.run(AutherApp.class, args);
     }
 
 }
